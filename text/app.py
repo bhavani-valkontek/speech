fc61393197c9780efb8216dcf6fbe7da988ba825
bhavani-valkontek <bhavani.babbili@valkontek.com>
2025-11-21 12:31:34 +0530

    Update app.py

--- text/app.py
+++ text/app.py
@@ -1,22 +1,127 @@
+# import streamlit as st
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+# from gtts import gTTS
+# from streamlit_extras.stylable_container import stylable_container
+# import base64
+
+# # Load model from Hugging Face Hub
+# MODEL_NAME = "babbilibhavani/nllb-translator"  # <-- Replace with your Hugging Face repo
+
+# @st.cache_resource
+# def load_model():
+#     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+#     model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME,from_pt=True)
+#     return tokenizer, model
+
+
+# tokenizer, model = load_model()
+
+# # Language codes
+# LANG_CODES = {
+#     "Hindi": "hin_Deva",
+#     "Bengali": "ben_Beng",
+#     "Marathi": "mar_Deva",
+#     "Telugu": "tel_Telu",
+#     "Tamil": "tam_Taml",
+#     "Gujarati": "guj_Gujr",
+#     "Urdu": "urd_Arab",
+#     "Kannada": "kan_Knda",
+#     "Malayalam": "mal_Mlym",
+#     "Punjabi": "pan_Guru",
+# }
+
+# # gTTS language map
+# gtts_lang_map = {
+#     "Hindi": "hi",
+#     "Bengali": "bn",
+#     "Marathi": "mr",
+#     "Telugu": "te",
+#     "Tamil": "ta",
+#     "Gujarati": "gu",
+#     "Urdu": "ur",
+#     "Kannada": "kn",
+#     "Malayalam": "ml",
+#     "Punjabi": "pa",
+# }
+
+# st.title("Language Translator")
+
+# text_input = st.text_area("Enter text")
+
+# col1, col2 = st.columns(2)
+# source_lang = col1.selectbox("Source", LANG_CODES.keys())
+# target_lang = col2.selectbox("Target", LANG_CODES.keys())
+
+# if st.button("Translate"):
+#     tokenizer.src_lang = LANG_CODES[source_lang]
+
+#     inputs = tokenizer(text_input, return_tensors="pt")
+#     output_tokens = model.generate(
+#         **inputs,
+#         forced_bos_token_id=tokenizer.lang_code_to_id[LANG_CODES[target_lang]],
+#     )
+#     translated_text = tokenizer.batch_decode(output_tokens, skip_special_tokens=True)[0]
+
+#     st.session_state["translated_text"] = translated_text
+
+#     # Generate audio
+#     tts = gTTS(translated_text, lang=gtts_lang_map[target_lang])
+#     audio_path = "translated_audio.mp3"
+#     tts.save(audio_path)
+
+#     with open(audio_path, "rb") as f:
+#         audio_bytes = f.read()
+
+#     st.session_state["audio_base64"] = base64.b64encode(audio_bytes).decode("utf-8")
+
+# if "translated_text" in st.session_state:
+#     st.text_area("Translated Text", st.session_state["translated_text"], height=150)
+
+#     # PLAY BUTTON WITHOUT REFRESH
+#     with stylable_container(
+#         key="play_button",
+#         css_styles="""
+#         button {
+#             background-color: #ffffff00;
+#             border: none;
+#             font-size: 40px;
+#             cursor: pointer;
+#         }
+#     """,
+#     ):
+#         if st.button("🔊Listen"):
+#             audio_html = f"""
+#                 <audio autoplay>
+#                     <source src="data:audio/mp3;base64,{st.session_state["audio_base64"]}">
+#                 </audio>
+#             """
+#             st.markdown(audio_html, unsafe_allow_html=True)
+
+
+
+
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from gtts import gTTS
 from streamlit_extras.stylable_container import stylable_container
 import base64
 
-# Load model from Hugging Face Hub
-MODEL_NAME = "babbilibhavani/nllb-translator"  # <-- Replace with your Hugging Face repo
+# -----------------------------
+# Load model from Hugging Face
+# -----------------------------
+MODEL_NAME = "your-username/nllb-translator"  # Replace with your HF repo
 
 @st.cache_resource
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
-    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME,from_pt=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
     return tokenizer, model
-
 
 tokenizer, model = load_model()
 
-# Language codes
+# -----------------------------
+# Language mappings
+# -----------------------------
 LANG_CODES = {
     "Hindi": "hin_Deva",
     "Bengali": "ben_Beng",
@@ -30,7 +135,6 @@ LANG_CODES = {
     "Punjabi": "pan_Guru",
 }
 
-# gTTS language map
 gtts_lang_map = {
     "Hindi": "hi",
     "Bengali": "bn",
@@ -44,6 +148,9 @@ gtts_lang_map = {
     "Punjabi": "pa",
 }
 
+# -----------------------------
+# Streamlit UI
+# -----------------------------
 st.title("Language Translator")
 
 text_input = st.text_area("Enter text")
@@ -52,7 +159,7 @@ col1, col2 = st.columns(2)
 source_lang = col1.selectbox("Source", LANG_CODES.keys())
 target_lang = col2.selectbox("Target", LANG_CODES.keys())
 
-if st.button("Translate"):
+if st.button("Translate") and text_input.strip():
     tokenizer.src_lang = LANG_CODES[source_lang]
 
     inputs = tokenizer(text_input, return_tensors="pt")
@@ -61,7 +168,6 @@ if st.button("Translate"):
         forced_bos_token_id=tokenizer.lang_code_to_id[LANG_CODES[target_lang]],
     )
     translated_text = tokenizer.batch_decode(output_tokens, skip_special_tokens=True)[0]
-
     st.session_state["translated_text"] = translated_text
 
     # Generate audio
@@ -74,6 +180,9 @@ if st.button("Translate"):
 
     st.session_state["audio_base64"] = base64.b64encode(audio_bytes).decode("utf-8")
 
+# -----------------------------
+# Show translated text & play audio
+# -----------------------------
 if "translated_text" in st.session_state:
     st.text_area("Translated Text", st.session_state["translated_text"], height=150)
 
@@ -81,18 +190,19 @@ if "translated_text" in st.session_state:
     with stylable_container(
         key="play_button",
         css_styles="""
-        button {
-            background-color: #ffffff00;
-            border: none;
-            font-size: 40px;
-            cursor: pointer;
-        }
-    """,
+            button {
+                background-color: #ffffff00;
+                border: none;
+                font-size: 40px;
+                cursor: pointer;
+            }
+        """,
     ):
-        if st.button("🔊Listen"):
+        if st.button("🔊 Listen"):
             audio_html = f"""
                 <audio autoplay>
                     <source src="data:audio/mp3;base64,{st.session_state["audio_base64"]}">
                 </audio>
             """
             st.markdown(audio_html, unsafe_allow_html=True)
+
